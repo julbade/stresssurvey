3ded8e868a2f7664211afe45d0df493ec5e92073
Julius Bade and Mark Mangahas <mark@>
2018-08-14 16:16:06 -0700

    finalized if and else statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,28 @@
+var stressLevel = 0;
+
 $(document).ready(function(){
   $("form#stress_survey").submit(function(event){
     event.preventDefault();
-
-    $("#work-responses").show();
     $("input:checkbox[name=work-stress]:checked").each(function(){
-      var workStress = $(this).val();
-      $('#work-responses').append(workStress + "<br>");
-    });
-    $("#health-responses").show();
-    $("input:checkbox[name=health-stress]:checked").each(function(){
-      var healthStress = $(this).val();
-      $('#health-responses').append(healthStress + "<br>");
-    });
-    $("#coping-method").show();
-    $("input:checkbox[name=coping-method]:checked").each(function(){
-        var copingMethod = $(this).val();
-        $('#coping-method').append(copingMethod + "<br>");
+      stressLevel++;
     });
 
-    $("#stress_survey").hide();
+    $("input:checkbox[name=health-stress]:checked").each(function(){
+      stressLevel++;
+    });
+
+    $("input:checkbox[name=coping-method]:checked").each(function(){
+      stressLevel--;
+    });
+
+    if (stressLevel === 0) {
+      alert("You are not stressed");
+    } else if (stressLevel > 0 && stressLevel <= 5 ) {
+      alert("You're not that stressed, quit bitching");
+    } else if (stressLevel > 5 && stressLevel <= 9) {
+      alert("You're kinda stressed. Keep doing those coping methods");
+    }
+
 
   });
 });
